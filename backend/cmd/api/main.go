2612f87ee377784d36e64c1e6f31d9fc31dc6cb8
Lytton <lytton.liao@gmail.com>
2024-04-03 18:57:16 -0400

    Allow for configuring the rate limiters

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -34,6 +34,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 // Define application struct to hold dependencies for our HTTP handlers, helpers, middleware
@@ -54,6 +59,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
 	flag.Parse()
 
 	// Initialize a new jsonlog.Logger which write snay messages *at or above* the INFO severity level to the stdout stream
--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -38,49 +38,58 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		limiter  *rate.Limiter
 		lastSeen time.Time
 	}
+
 	var (
 		mu      sync.Mutex
 		clients = make(map[string]*client)
 	)
 
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) < 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
+
 	// The function we return is a closure, which closes over the limiter variable
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			app.serverErrorResponse(w, r, err)
-			return
-		}
-
-		go func() {
-			for {
-				time.Sleep(time.Minute)
-
-				mu.Lock()
-
-				for ip, client := range clients {
-					if time.Since(client.lastSeen) > 3*time.Minute {
-						delete(clients, ip)
-					}
-				}
-
-				mu.Unlock()
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
 			}
-		}()
 
-		// Lock the mutex to prevent this code from being executed concurrently
-		mu.Lock()
+			mu.Lock()
 
-		if _, found := clients[ip]; !found {
-			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
-		}
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(
+						rate.Limit(app.config.limiter.rps),
+						app.config.limiter.burst,
+					),
+				}
+			}
 
-		if !clients[ip].limiter.Allow() {
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
 			mu.Unlock()
-			app.rateLimitExceededResponse(w, r)
-			return
 		}
-
-		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
