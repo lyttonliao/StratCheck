b67d657ef7bdbb53d740ff65e561096b4424a856
Lytton <lytton.liao@gmail.com>
2024-04-03 16:42:52 -0400

    Created a custom logger

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/lyttonliao/StratCheck/internal/jsonlog"
+
 	"github.com/lyttonliao/StratCheck/internal/data"
 
 	// Alias this import to blank identifier to stop Go compiler from erroring
@@ -37,7 +39,7 @@ type config struct {
 // Define application struct to hold dependencies for our HTTP handlers, helpers, middleware
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -54,19 +56,19 @@ func main() {
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 	flag.Parse()
 
-	// Initialize a new logger which writes message to standard out stream
-	// prefixed with current date and time
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	// Initialize a new jsonlog.Logger which write snay messages *at or above* the INFO severity level to the stdout stream
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	// Call the openDB() helper function to create the connection pool, passing in the config struct
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	// Defer a call to db.Close() so that the connection pool is closed before exiting main() function
 	defer db.Close()
-	logger.Printf("database connection established")
+
+	logger.PrintInfo("database connection pool established", nil)
 
 	// Declare an instance of the application struct, containing the config struct and logger
 	app := &application{
@@ -77,18 +79,25 @@ func main() {
 
 	// Declare a HTTP server with timeout settings, which listens on the port
 	// provided in the config struct and uses servemux as the handler
+	// Create a new Go log.Logger instance, The "" and 0 indicate that the
+	// log.Logger instance should not use a prefix or any flags
+	// Any log messages that http.Server writes will be passed to our Logger.Write() method
+	// because our Logger type satisfies the io.Writer interface (due to Write() method)
 	srv := http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		ErrorLog:     log.New(logger, "", 0),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	// Start the HTTP server
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": cfg.env,
+		"env":  srv.Addr,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
